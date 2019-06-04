7466b0334a39a165349e122004c51de3dd2932ea
Drew <Drew@DESKTOP-VMJES9Q>
2019-06-04 20:30:36 +1000

    Added Dark Mode Toggle

--- AgileAssignment3/src/MainPackage/MainClass.java
+++ AgileAssignment3/src/MainPackage/MainClass.java
@@ -2,17 +2,24 @@ package MainPackage;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.*;
 import javax.swing.*;
-import java.util.*;
+import java.util.*; 
+import java.awt.Color;
 
 class MainClass{
+	public static final Color VERY_DARK_GRAY = new Color(51,51,51);
+	public static final Color LIGHT_GRAY = new Color(153,153,153);
+	public static final Color VERY_LIGHT_GRAY = new Color(217,217,217);
+	
     public static void main(String args[]){
     	//Create the ArrayList
     	ArrayList<String> ArrayList=new ArrayList<String>();
     	
     	//Creating the Frame
-        JFrame frame = new JFrame("Agile Coding Assignment 3!");
+        JFrame frame = new JFrame("Chat Frame");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 600);
 
@@ -22,7 +29,11 @@ class MainClass{
         //Create Top Panel
         JPanel toppanel = new JPanel();
         
+        // Create Text Area at the Center
+        JTextArea textarea = new JTextArea();
+        
         //Create Bottom Panel Components
+        JLabel darkmodelabel = new JLabel("Enable Dark Mode");
         JLabel entertextlabel = new JLabel("Enter List Element");
         JTextField textfield = new JTextField(15); //Limit to 15 Char
         JButton send = new JButton("Add");
@@ -54,17 +65,51 @@ class MainClass{
         	}
         });
         
+        //Create Top Panel Components
+        JCheckBox DarkMode = new JCheckBox();
+        DarkMode.addItemListener(new ItemListener() {
+
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if(DarkMode.isSelected()) {
+                	frame.setBackground(Color.DARK_GRAY);
+                	bottompanel.setBackground(VERY_DARK_GRAY);
+                	toppanel.setBackground(VERY_DARK_GRAY);
+                	DarkMode.setBackground(VERY_DARK_GRAY);
+                	textfield.setBackground(Color.GRAY);
+                	textarea.setBackground(Color.GRAY);
+                	darkmodelabel.setForeground(Color.WHITE);
+                	entertextlabel.setForeground(Color.WHITE);
+                	System.out.println("Sel");
+                }
+                else {
+                	frame.setBackground(LIGHT_GRAY);
+                	bottompanel.setBackground(VERY_LIGHT_GRAY);
+                	toppanel.setBackground(VERY_LIGHT_GRAY);
+                	textfield.setBackground(Color.WHITE);
+                	textarea.setBackground(Color.WHITE);
+                	DarkMode.setBackground(VERY_LIGHT_GRAY);
+                	darkmodelabel.setForeground(Color.BLACK);
+                	entertextlabel.setForeground(Color.BLACK);
+                	System.out.println("DeSel");
+                }
+            }
+         
+        });
+        
         
         //Add Components to Bottom Panel (flow layout)
         bottompanel.add(entertextlabel); 
         bottompanel.add(textfield);
         bottompanel.add(send);
         bottompanel.add(clearall);
-        
-        // Create Text Area at the Center
-        JTextArea textarea = new JTextArea();
+
+        //Add Components to Top Panel (flow layout)
+        toppanel.add(darkmodelabel); 
+        toppanel.add(DarkMode);
 
         //Adding Components to the frame.
+        frame.getContentPane().add(BorderLayout.NORTH, toppanel);
         frame.getContentPane().add(BorderLayout.SOUTH, bottompanel);
         frame.getContentPane().add(BorderLayout.CENTER, textarea);
         frame.setVisible(true);
